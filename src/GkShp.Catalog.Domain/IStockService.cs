7ff3a5064fa80f3f30547db88946cab427b2264e
Fernando Vilhena <fernando.vilhena@iatec.com>
2022-04-26 01:03:38 -0300

    Using domain services

---
+++ src/GkShp.Catalog.Domain/IStockService.cs
@@ -0,0 +1,8 @@
+﻿namespace GkShp.Catalog.Domain
+{
+    public interface IStockService : IDisposable
+    {
+        Task<bool> DebitStock(Guid productId, int quantity);
+        Task<bool> ReplaceStock(Guid productId, int quantity);
+    }
+}
---
+++ src/GkShp.Catalog.Domain/StockService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GkShp.Catalog.Domain
+{
+    public class StockService : IStockService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public StockService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> DebitStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetByIdAsync(productId);
+            
+            if (product == null) return false;
+
+            if (!product.HaveStock(quantity)) return false;
+
+            product.DebitStock(quantity);
+
+            _productRepository.Update(product);
+
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReplaceStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetByIdAsync(productId);
+
+            if (!product.HaveStock(quantity)) return false;
+
+            product.DebitStock(quantity);
+
+            _productRepository.Update(product);
+
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _productRepository?.Dispose();
+        }
+    }
+}
